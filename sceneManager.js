0f1a369ded7450853dc78401e65c4a03d2fd0afa
Ken Ahrens <75004340+Kenpai718@users.noreply.github.com>
2022-01-19 21:06:16 -0800

    Fixed keyboard debugger after andre changed the input detection

--- sceneManager.js
+++ sceneManager.js
@@ -40,43 +40,42 @@ class SceneManager {
     viewDebug(ctx) {;
         // left debug
         ctx.lineWidth = 2;
-        ctx.strokeStyle = this.game.keys[this.player.left] ? "Red" : "SpringGreen";
+        ctx.strokeStyle = this.game.left ? "Red" : "SpringGreen";
         ctx.fillStyle = ctx.strokeStyle;
-        console.log(this.game.surfaceHeight);
         ctx.strokeRect(10, this.game.surfaceHeight - 40, 30, 30);
         ctx.fillText("A", 20, this.game.surfaceHeight - 20);
 
         // down debug
         ctx.lineWidth = 2;
-        ctx.strokeStyle = this.game.keys[this.player.down] ? "Red" : "SpringGreen";
+        ctx.strokeStyle = this.game.down ? "Red" : "SpringGreen";
         ctx.fillStyle = ctx.strokeStyle;
         ctx.strokeRect(50, this.game.surfaceHeight -40, 30, 30);
         ctx.fillText("S", 60, this.game.surfaceHeight - 20);
 
         // up debug
         ctx.lineWidth = 2;
-        ctx.strokeStyle = this.game.keys[this.player.up]  ? "Red" : "SpringGreen";
+        ctx.strokeStyle = this.game.up ? "Red" : "SpringGreen";
         ctx.fillStyle = ctx.strokeStyle;
         ctx.strokeRect(50, this.game.surfaceHeight -80, 30, 30);
         ctx.fillText("W", 60, this.game.surfaceHeight - 60);
 
         // right debug
         ctx.lineWidth = 2;
-        ctx.strokeStyle = this.game.keys[this.player.right]  ? "Red" : "SpringGreen";
+        ctx.strokeStyle = this.game.right  ? "Red" : "SpringGreen";
         ctx.fillStyle = ctx.strokeStyle;
         ctx.strokeRect(90, this.game.surfaceHeight -40, 30, 30);
         ctx.fillText("D", 100, this.game.surfaceHeight - 20);
 
         // jump debug
         ctx.lineWidth = 2;
-        ctx.strokeStyle = this.game.keys[this.player.jump]  ? "Red" : "SpringGreen";
+        ctx.strokeStyle = this.game.jump  ? "Red" : "SpringGreen";
         ctx.fillStyle = ctx.strokeStyle;
         ctx.strokeRect(130, this.game.surfaceHeight -40, 50, 30);
         ctx.fillText("SPACE", 140, this.game.surfaceHeight - 20);
 
 		// roll debug
 		ctx.lineWidth = 2;
-		ctx.strokeStyle = this.game.keys[this.player.roll]  ? "Red" : "SpringGreen";
+		ctx.strokeStyle = this.game.roll ? "Red" : "SpringGreen";
 		ctx.fillStyle = ctx.strokeStyle;
 		ctx.strokeRect(130, this.game.surfaceHeight - 80, 50, 30);
 		ctx.fillText("LSHIFT", 140, this.game.surfaceHeight - 60);  
@@ -84,7 +83,7 @@ class SceneManager {
         // attack debug
     
         ctx.lineWidth = 2;
-        ctx.strokeStyle = this.game.keys[this.player.attack]  ? "Red" : "SpringGreen";
+        ctx.strokeStyle = this.game.attack  ? "Red" : "SpringGreen";
         ctx.fillStyle = ctx.strokeStyle;
         ctx.strokeRect(190, this.game.surfaceHeight -40, 30, 30);
         ctx.fillText("P", 200, this.game.surfaceHeight - 20);
