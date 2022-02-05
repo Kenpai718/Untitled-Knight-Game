d794a4c8a2e495425f7545157ef6f19ee8a56d62
Ninjasaws <44685782+Ninjasaws@users.noreply.github.com>
2022-02-05 11:58:07 -0800

    Chest GUI
    - Chest now displays GUI for # of items earned (hp potions / arrows)
     - Chest GUI fades then disappears within 10 seconds of being opened
    - Random Generated numbers for # of items earned (might change later)
    
    TO DO: Add # of gained items to players inventory

--- entity/objects/chest.js
+++ entity/objects/chest.js
@@ -3,20 +3,26 @@ class Chest {
         
         Object.assign(this, {game, x, y});
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/environment/dark_castle_tileset.png");
+
+        this.arrowStorage = 0; // Gives random amount of arrows 1-15
+        this.healthStorage = 0; // Gives random amount of hp potions 1-3
         
         // Mob sizes
-        this.scale = 4; // 2.5
+        this.scale = 4; // 4
         this.width = 21 * this.scale;
         this.height = 12 * this.scale;
 
         // Update settings
         this.tick = 0;
+        this.timerGUI = 0;
+        this.timerGUI2 = 0;
 
         // Mapping animations and states
         this.states = {closed: 0, opened: 1};
         this.animations = []; // [state][direction]
 
         this.state = 0;
+        this.opened = false;
 
         // Boundary box.
         this.BB = null;
@@ -43,14 +49,27 @@ class Chest {
 
     update() {
 
+
+
+        // If Chest is hit by player, change to Opened state
         let that = this;
         this.game.entities.forEach(function (entity) {
-            if (entity.HB && that.BB.collide(entity.HB) && entity instanceof AbstractPlayer) {
+            if (entity.HB && that.BB.collide(entity.HB) && entity instanceof AbstractPlayer && that.state != 1) {
                 that.state = 1;
+                //console.log(that.arrowStorage);
+                //console.log(that.healthStorage);
+                //console.log(that.healthStorage - that.healthStorage);
+                //console.log(that.arrowStorage - that.arrowStorage);
+                that.healthStorage = 1 + Math.floor(Math.random() * 3); // Gives random amount of hp potions 1-3
+                that.arrowStorage = 1 + Math.floor(Math.random() * 15); // Gives random amount of arrows 1-15
+
+                that.opened = true;
+                that.timerGUI = that.timerGUI2 + 1;
             }
         });
-        
-        
+        if(that.opened && that.timerGUI2 < 10 )
+            that.timerGUI2 += that.game.clockTick;
+
     };
 
     loadAnimations() {
@@ -62,8 +81,10 @@ class Chest {
 
         // Animations  [state]
 
-        // Idle Animation
-        this.animations[0] = new Animator(this.spritesheet, 19, 147, 21, 12, 1, 0, 0, 0, 0, 0);    // 0.14 Animation Speed 
+        // Closed state
+        this.animations[0] = new Animator(this.spritesheet, 19, 147, 21, 12, 1, 0, 0, 0, 0, 0);
+        
+        // Opened state
         this.animations[1] = new Animator(this.spritesheet, 51, 147, 21, 12, 1, 0, 0, 0, 0, 0);
 
     };
@@ -79,6 +100,28 @@ class Chest {
             case 1: // Opened chest
                 this.animations[this.state].drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
                 break;
+        }
+
+        // Once opened, Chest will display # of earned items from the chest
+        let that = this;
+        if(that.opened) {
+            ctx.font = PARAMS.BIG_FONT;
+            let tempColor = ctx.fillStyle;
+            ctx.fillStyle = "White"
+
+            if(that.timerGUI2 >= 10)
+                ctx.globalAlpha = 0;
+            else
+                ctx.globalAlpha = that.timerGUI / that.timerGUI2;
+            
+            
+            ctx.fillText("🏹 x" + that.arrowStorage, that.x, that.y - 5);
+            ctx.fillText("⚗️ x" + that.healthStorage, that.x, that.y - 40);
+            
+            
+            ctx.font = PARAMS.DEFAULT_FONT;
+            ctx.fillStyle = tempColor;
+            ctx.globalAlpha = 1;
         }
 
         if (PARAMS.DEBUG) {
