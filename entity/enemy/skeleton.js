e28217873d3017a25ad91dfe0945b71a34b31aef
Ninjasaws <44685782+Ninjasaws@users.noreply.github.com>
2022-02-06 14:19:45 -0800

    Skeleton AI update

--- entity/enemy/skeleton.js
+++ entity/enemy/skeleton.js
@@ -1,190 +1,215 @@
-class Skeleton {
+const SKELETON = {
+    NAME: "Skeleton",
+    MAX_HP: 100,
+    SCALE: 2.5,
+    WIDTH: 33 * this.scale,
+    HEIGHT: 51 * this.scale,
+    DAMAGE: 20
+};
+
+class Skeleton extends AbstractEnemy {
 
     constructor(game, x, y){
         
-        Object.assign(this, {game, x, y});
+        super(game, x, y, STATS.SKELETON.NAME,  STATS.SKELETON.MAX_HP, STATS.SKELETON.WIDTH, STATS.SKELETON.HEIGHT, STATS.SKELETON.SCALE);
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/enemy/skeleton.png");
 
-        // Mob scaling sizes - Currently no width
-        this.scale = 2.5; // 2.5
-        this.height = 51 * this.scale;
+       // Update settings
+       this.tick = 0;
+       this.seconds = 0;
+       this.doRandom = 0;
+       this.alert = false; // enemy is near or got hit
 
-        // Update settings
-        this.tick = 0;
-        this.seconds = 0;
-        this.doRandom = 0;
-        this.alert = false; // enemy is near or got hit
+       // Physics
+       this.fallAcc = 1500;
+       this.collisions = {left: false, right: false, top: false, bottom: false};
 
-        this.velocity = { x: 0, y: 0 };
-        this.fallAcc = 1500;
-        this.collisions = {left: false, right: false, top: false, bottom: false};
+       //variables to control behavior
+       this.canAttack = true;
+       this.vulnerable = true;
+       this.damagedCooldown = 0;
+       this.runAway = false;
+       this.attackCooldown = 0;
 
-        // Mapping charater states for animations
-        this.states = {idle: 0, damaged: 1, death: 2, attack: 3, move: 4, block: 5};
-        this.directions = {left: 0, right: 1 };
-        this.animations = []; // [state][direction] 
-        
-        this.state = 0;
-        this.direction = 0;
+       this.damageValue = STATS.SKELETON.DAMAGE;
 
-        // Hit, Attack, and View boxes.
-        this.BB = null;
-        this.AB = null;
-        this.VB = null;
-
-        // When Debug box is true, select mob box to display
-        this.displayBoundingbox = true;
-        this.displayAttackbox = true;
-        this.displayVisionbox = true;
+       // Mapping animations and mob states
+       this.animations = []; // [state][direction]
+       this.states = {idle: 0, damaged: 1, death: 2, attack: 3, move: 4, run: 5};
+       this.directions = {left: 0, right: 1 };
+       this.direction = this.directions.left;
+       this.state = this.states.idle;
 
         // Other
         this.loadAnimations();
-        this.updateBB();
+        this.updateBoxes();
     };
 
-    updateBB() {
-        this.lastHitBox = this.BB;
+    resetAnimationTimers(action) {
+        this.animations[action][0].elapsedTime = 0;
+        this.animations[action][1].elapsedTime = 0;
+    }
+
+    getDamageValue() {
+        return this.damageValue;
+    };
+
+    setDamagedState() {
+        this.vulnerable = false;
+        this.state = this.states.damaged;
+    };
+
+    updateHB() {
+        if (this.direction == 0)    this.AR = new BoundingBox(this.x - 109, this.y-53, this.attackwidth, 57 * this.scale)
+        else                        this.AR = new BoundingBox(this.x + 32, this.y-53, this.attackwidth, 57 * this.scale)
+    };
+
+    updateBoxes() {
         this.BB = new BoundingBox(this.x + 14 * this.scale, this.y-37, 21 * this.scale, 51 * this.scale)
-        this.updateAB();
-        this.updateVB();
-    }
-
-    updateAB() {
-        this.lastAttackbox = this.AB;
-        if (this.direction == 0)    this.AB = new BoundingBox(this.x - 109, this.y-53, this.attackwidth, 57 * this.scale)
-        else                        this.AB = new BoundingBox(this.x + 32, this.y-53, this.attackwidth, 57 * this.scale)
-    }
-
-    updateVB() {
-        this.lastVisionbox = this.VB;
+        if (this.direction == 0)    this.AR = new BoundingBox(this.x - 109, this.y-53, this.attackwidth, 57 * this.scale)
+        else                        this.AR = new BoundingBox(this.x + 32, this.y-53, this.attackwidth, 57 * this.scale)
         this.VB = new BoundingBox(this.x + 62 - this.visionwidth/2, this.y-37, this.visionwidth, this.height)
+
     }
 
     viewBoundingBox(ctx) { 
-        if(this.displayBoundingbox) {        // This is the Bounding Box, defines space where mob can be hit
-            ctx.strokeStyle = "Red";
-            ctx.strokeRect(this.x + 14 * this.scale, this.y-37, 21 * this.scale, 51 * this.scale);
-        }
-        if(this.displayAttackbox) {     // This is Attack Box, defines mob attack area
-            ctx.strokeStyle = "Orange";
-            this.attackwidth = 200;
-            if (this.direction == 0)    ctx.strokeRect(this.x - 109, this.y-53, this.attackwidth, 57 * this.scale);
-            else                        ctx.strokeRect(this.x + 32, this.y-53, this.attackwidth, 57 * this.scale);
-        }
-        if(this.displayVisionbox) {      // This is Vision Box, allows mob to see player when it collides with player's hitbox
-            this.visionwidth = 1200;
-            ctx.strokeStyle = "Yellow";
-            ctx.strokeRect(this.x + 62 - this.visionwidth/2, this.y-37, this.visionwidth, this.height);
-        }
+        // This is the Bounding Box, defines space where mob can be hit
+        ctx.strokeStyle = "Red";
+        ctx.strokeRect(this.x + 14 * this.scale, this.y-37, 21 * this.scale, 51 * this.scale);
+        
+        // This is Attack Box, defines mob attack area
+        ctx.strokeStyle = "Orange";
+        this.attackwidth = 200;
+        if (this.direction == 0)    ctx.strokeRect(this.x - 109, this.y-53, this.attackwidth, 57 * this.scale);
+        else                        ctx.strokeRect(this.x + 32, this.y-53, this.attackwidth, 57 * this.scale);
+        
+        // This is Vision Box, allows mob to see player when it collides with player's hitbox
+        this.visionwidth = 1200;
+        ctx.strokeStyle = "Yellow";
+        ctx.strokeRect(this.x + 62 - this.visionwidth/2, this.y-37, this.visionwidth, this.height);
+        
     };
 
     update() {
 
         this.seconds += this.game.clockTick;
 
-        // All this aint me vv
+
         const TICK = this.game.clockTick;
-        const SCALER = 1;
-        //currently using Chris Marriot's mario physics
-        const MIN_WALK = 1 * SCALER;
-        const MAX_WALK = 93.75 * SCALER;
-        const MAX_RUN = 153.75 * SCALER;
-        const ACC_WALK = 133.59375 * SCALER;
+        const SCALER = 3;
+        const MAX_RUN = 123 * SCALER;
         const ACC_RUN = 200.390625 * SCALER;
-        const DEC_REL = 182.8125 * SCALER;
-        const DEC_SKID = 365.625 * SCALER;
-        const MIN_SKID = 33.75 * SCALER;
-        const ROLL_SPD = 400 * SCALER;
-        const CROUCH_SPD = 50 * SCALER;
-        const DOUBLE_JUMP_X_BOOST = 10;
-        const STOP_FALL = 1575;
-        const WALK_FALL = 1800;
-        const RUN_FALL = 2025;
-        const STOP_FALL_A = 450;
-        const WALK_FALL_A = 421.875;
-        const RUN_FALL_A = 562.5;
-        const JUMP_HEIGHT = 1500;
-        const DOUBLE_JUMP_HEIGHT = 550;
         const MAX_FALL = 270 * SCALER;
 
-        this.velocity.y += this.fallAcc * TICK;
+        if (this.dead) { // skeleton is dead play death animation and remove
+            this.healthbar.removeFromWorld = true;
+            this.state = this.states.death;
+            this.HB = null; // so it cant attack while dead
+            if (this.animations[this.state][this.direction].isDone()) {
+                this.removeFromWorld = true;
+            }
 
-        // max y velocity
-        if (this.velocity.y >= MAX_FALL) this.velocity.y = MAX_FALL;
-        if (this.velocity.y <= -MAX_FALL) this.velocity.y = -MAX_FALL;
+        } else { // not dead keep moving
 
-        //max x velocity
-        let doubleJumpBonus = 0;
-        if (!this.doubleJump) doubleJumpBonus = DOUBLE_JUMP_X_BOOST;
-        if (this.velocity.x >= MAX_RUN) this.velocity.x = MAX_RUN + doubleJumpBonus;
-        if (this.velocity.x <= -MAX_RUN) this.velocity.x = -MAX_RUN - doubleJumpBonus;
+            this.velocity.y += this.fallAcc * TICK;
+            if (this.velocity.y >= MAX_FALL) this.velocity.y = MAX_FALL;
+            if (this.velocity.y <= -MAX_FALL) this.velocity.y = -MAX_FALL;
+            if (this.velocity.x >= MAX_RUN) this.velocity.x = MAX_RUN;
+            if (this.velocity.x <= -MAX_RUN) this.velocity.x = -MAX_RUN;
 
-        //update position and bounding box
-        this.x += this.velocity.x * TICK;
-        this.y += this.velocity.y * TICK;
-        this.updateBB();
+            this.x += this.velocity.x * TICK;
+            this.y += this.velocity.y * TICK;
+            this.updateBoxes();
 
-        // Collision variables
-        this.collisions = {left: false, right: false, top: false, bottom: false};
-        let dist = { x : 0, y : 0};
-        let hole = 0; // at most 15, floor/ceil = 8, adj floor/ceil = 4, low wall = 2, high wall = 1
-        let that = this;
-        let knightInSight = false;
-
-        // Collisions
-        this.game.entities.forEach(function (entity) {
-            if (entity.BB && that.BB.collide(entity.BB)) {
-                if (that.BB.top < entity.BB.top && that.BB.bottom > entity.BB.top) { 
-                    that.collisions.bottom = true;
-                    dist.y = entity.BB.top - that.BB.bottom;
+            let dist = { x: 0, y: 0 };
+            let that = this;
+            let knightInSight = false;
+            this.collisions = { left: false, right: false, top: false, bottom: false };
+            this.game.entities.forEach(function (entity) {
+                // collision with environment
+                if (entity.BB && that.BB.collide(entity.BB) && (entity instanceof Ground || entity instanceof Walls || entity instanceof Platform || entity instanceof Brick)) {
+                    if (that.BB.top < entity.BB.top && that.BB.bottom > entity.BB.top) {
+                        if (that.BB.left < entity.BB.left && Math.abs(that.BB.right - entity.BB.left) <= Math.abs(that.BB.bottom - entity.BB.top)) {
+                            that.collisions.right = true;
+                            dist.x = entity.BB.left - that.BB.right;
+                        } else if (that.BB.right > entity.BB.right && Math.abs(that.BB.left - entity.BB.right) <= Math.abs(that.BB.bottom - entity.BB.top)) {
+                            that.collisions.left = true;
+                            dist.x = entity.BB.right - that.BB.left;
+                        } else {
+                            that.collisions.bottom = true;
+                            dist.y = entity.BB.top - that.BB.bottom;
+                        }
+                    }
+                    that.updateBoxes();
                 }
-                that.updateBB();
+                // knight is in the vision box
+                if (entity.BB && entity instanceof Knight && that.VB.collide(entity.BB)) {
+                    knightInSight = true;
+                    // knight is in the vision box and not in the attack range
+                    if (!that.AR.collide(entity.BB)) {
+                        // move towards the knight
+                        that.state = that.states.move;
+                        that.direction = entity.BB.right < that.BB.left ? that.directions.left : that.directions.right;
+                        that.velocity.x = that.direction == that.directions.right ? that.velocity.x + MAX_RUN : that.velocity.x - MAX_RUN;
+                    }
+                }
+                // knight is in attack range
+                if (entity.BB && entity instanceof Knight && that.AR.collide(entity.BB)) {
+                    that.velocity.x = 0;
+                    if (that.canAttack || !that.animations[that.states.attack][that.direction].isDone()) {
+                        that.runAway = true;
+                        that.canAttack = false;
+                        that.state = that.states.attack;
+                    }
+                }
+
+                // skeleton hit by something switch the state to damaged
+                if (entity.HB && that.BB.collide(entity.HB) && entity instanceof AbstractPlayer && !that.HB) {
+                    //entity.doDamage(that);
+                    that.setDamagedState();
+
+                } else if (entity.BB && that.BB.collide(entity.BB) && entity instanceof Arrow && !that.HB) {
+                    that.setDamagedState();
+                }
+
+            });
+            // update positions based on environment collisions
+            this.x += dist.x;
+            this.y += dist.y;
+            this.updateBoxes();
+            // set respective velocities to 0 for environment collisions
+            if (this.collisions.bottom && this.velocity.y > 0) this.velocity.y = 0;
+            if (this.collisions.left && this.velocity.x < 0) this.velocity.x = 0;
+            if (this.collisions.right && this.velocity.x > 0) this.velocity.x = 0;
+            // skeleton attack cooldown
+            if (!this.canAttack) {
+                this.attackCooldown += TICK;
+                if (this.attackCooldown >= 1.7) {
+                    this.resetAnimationTimers(this.states.attack);
+                    this.attackCooldown = 0;
+                    this.canAttack = true;
+                    this.runAway = false;
+                }
             }
-            if (entity.BB && that.VB.collide(entity.BB) && !that.AB.collide(entity.BB) && entity instanceof Knight) {
-                knightInSight = true;
-                that.state = that.states.move;
-                that.direction = entity.BB.right < that.BB.left ? that.directions.left : that.directions.right;
+            // skeleton hit cooldown
+            if (!this.vulnerable) {
+                this.damagedCooldown += TICK;
+                if (this.damagedCooldown >= PARAMS.DMG_COOLDOWN) {
+                    this.resetAnimationTimers(this.states.damaged);
+                    this.damagedCooldown = 0;
+                    this.canAttack = true;
+                    this.runAway = false;
+                    this.vulnerable = true;
+                }
             }
-            if (entity.BB && that.VB.collide(entity.BB) && that.AB.collide(entity.BB) && entity instanceof Knight) {
-                knightInSight = true;
-                that.state = that.states.attack;
-                that.direction = entity.BB.right < that.BB.left ? that.directions.left : that.directions.right;
-            }
-        });
-        this.y += dist.y;
-        this.updateBB();
-        if (this.collisions.bottom) {
-            if (this.velocity.y > 0) {
-                this.velocity.y = 0;
+            // deleted HB when not attacking
+            if (this.state != this.states.attack) this.HB = null;
+            // Do something random when player isnt in sight 
+            if (!knightInSight) {
+                this.state = this.states.idle;
+                this.velocity.x = 0;
             }
         }
-        /*
-        //Do something random player isnt in sight 
-        if(!knightInSight){
-            if(this.seconds >= this.doRandom){
-                this.doRandom = this.seconds + Math.floor(Math.random() * 3);
-                this.direction = Math.floor(Math.random() * 2);
-                this.action = Math.floor(Math.random() * 6);
-                if(this.action <= 1) {
-                    this.doRandom = this.seconds + Math.floor(Math.random() * 3);
-                    this.state = 4;
-                }
-                else {
-                    this.state = 0;
-                }
-            }
-        }
-        
-
-
-        // update velocity
-
-        if(that.state = 4 && knightInSight) {
-            if (this.direction == 0)    that.velocity.x -= MIN_WALK;
-            else                        that.velocity.x += MIN_WALK;
-        }
-        */
-        
         
 
     };
@@ -228,6 +253,14 @@ class Skeleton {
     };
 
     draw(ctx) {
+
+        if (this.dead) {
+            if (this.flickerFlag) {
+                this.animations[this.state][this.direction].drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y - this.game.camera.y, this.scale);
+            }
+            this.flickerFlag = !this.flickerFlag;
+        } else {
+            this.healthbar.draw(ctx); //only show healthbar when not dead
         
         switch(this.state) {
             case 0: // Idle
@@ -260,6 +293,7 @@ class Skeleton {
                         this.animations[this.state][this.direction].drawFrame(this.game.clockTick, ctx, this.x + 7, this.y-24, this.scale);
                 else    this.animations[this.state][this.direction].drawFrame(this.game.clockTick, ctx, this.x + 14, this.y-24, this.scale);
                 break;
+            }
         }
 
         if (PARAMS.DEBUG) {
