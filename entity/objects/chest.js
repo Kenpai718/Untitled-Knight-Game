a6ad2e84a01457923b8e9654c0a936914037014d
Ninjasaws <44685782+Ninjasaws@users.noreply.github.com>
2022-02-05 16:04:47 -0800

    GUI, Chest -> Player inventory
    - Now successfully added items to player inventory
    
    - Fixed chest following camera with GUI as well

--- entity/objects/chest.js
+++ entity/objects/chest.js
@@ -4,16 +4,16 @@ class Chest {
         Object.assign(this, {game, x, y});
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/environment/dark_castle_tileset.png");
 
-        this.arrowStorage = 0; // Gives random amount of arrows 1-15
-        this.healthStorage = 0; // Gives random amount of hp potions 1-3
-        
-        // Mob sizes
+        // Items in chest
+        this.arrowStorage = 0; // 0 until player interacts with chest
+        this.potionStorage = 0; 
+
+        // Scale sizes
         this.scale = 4; // 4
         this.width = 21 * this.scale;
         this.height = 12 * this.scale;
 
         // Update settings
-        this.tick = 0;
         this.timerGUI = 0;
         this.timerGUI2 = 0;
 
@@ -23,9 +23,7 @@ class Chest {
 
         this.state = 0;
         this.opened = false;
-
-        // Boundary box.
-        this.BB = null;
+        this.collected = false;
 
         // When Debug box is true, select boundary box to display
         this.displayBoundingbox = true;
@@ -41,35 +39,35 @@ class Chest {
     };
 
     viewBoundingBox(ctx) { 
-        if(this.displayBoundingbox) {        // This is the Bounding Box, defines space where chest is and can be opened
-            ctx.strokeStyle = "Red";
-            ctx.strokeRect(this.x, this.y,this.width, this.height);
-        }
+        // This is the Bounding Box, defines space where chest is and can be opened
+        ctx.strokeStyle = "Red";
+        ctx.strokeRect(this.x - this.game.camera.x, this.y - this.game.camera.y, this.width, this.height);
     };
 
     update() {
-
-
 
         // If Chest is hit by player, change to Opened state
         let that = this;
         this.game.entities.forEach(function (entity) {
             if (entity.HB && that.BB.collide(entity.HB) && entity instanceof AbstractPlayer && that.state != 1) {
                 that.state = 1;
-                //console.log(that.arrowStorage);
-                //console.log(that.healthStorage);
-                //console.log(that.healthStorage - that.healthStorage);
-                //console.log(that.arrowStorage - that.arrowStorage);
-                that.healthStorage = 1 + Math.floor(Math.random() * 3); // Gives random amount of hp potions 1-3
-                that.arrowStorage = 1 + Math.floor(Math.random() * 15); // Gives random amount of arrows 1-15
-
                 that.opened = true;
+
+                // varaibles needed for GUI to display amount
+                that.potionStorage = 1 + Math.floor(Math.random() * 3);      // Gives random amount of hp potions 1-3
+                that.arrowStorage = 1 + Math.floor(Math.random() * 15);     // Gives random amount of arrows 1-15
+
+                entity.myInventory.potions += that.potionStorage; 
+                entity.myInventory.arrows += that.arrowStorage;
+
+                console.log("Arrows: " + that.arrowStorage);
+                console.log("Potion: " + that.potionStorage);
+
                 that.timerGUI = that.timerGUI2 + 1;
             }
-        });
+        }); // Allows timer to start when open, used for fade effect
         if(that.opened && that.timerGUI2 < 10 )
             that.timerGUI2 += that.game.clockTick;
-
     };
 
     loadAnimations() {
@@ -83,7 +81,6 @@ class Chest {
 
         // Closed state
         this.animations[0] = new Animator(this.spritesheet, 19, 147, 21, 12, 1, 0, 0, 0, 0, 0);
-        
         // Opened state
         this.animations[1] = new Animator(this.spritesheet, 51, 147, 21, 12, 1, 0, 0, 0, 0, 0);
 
@@ -95,28 +92,29 @@ class Chest {
 
         switch(this.state) {
             case 0: // Closed chest
-                this.animations[this.state].drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
+                this.animations[this.state].drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y - this.game.camera.y, this.scale);
                 break;
             case 1: // Opened chest
-                this.animations[this.state].drawFrame(this.game.clockTick, ctx, this.x, this.y, this.scale);
+                this.animations[this.state].drawFrame(this.game.clockTick, ctx, this.x - this.game.camera.x, this.y - this.game.camera.y, this.scale);
                 break;
         }
 
         // Once opened, Chest will display # of earned items from the chest
+        // After a few seconds this display will fade and disappear after 10 seconds
         let that = this;
         if(that.opened) {
             ctx.font = PARAMS.BIG_FONT;
             let tempColor = ctx.fillStyle;
             ctx.fillStyle = "White"
 
-            if(that.timerGUI2 >= 10)
+            if(that.timerGUI2 >= 10) // Fading effect for chest GUI
                 ctx.globalAlpha = 0;
             else
                 ctx.globalAlpha = that.timerGUI / that.timerGUI2;
             
             
-            ctx.fillText("🏹 x" + that.arrowStorage, that.x, that.y - 5);
-            ctx.fillText("⚗️ x" + that.healthStorage, that.x, that.y - 40);
+            ctx.fillText("🏹 x" + that.arrowStorage, that.x - this.game.camera.x, that.y - 5 - this.game.camera.y);
+            ctx.fillText("⚗️ x" + that.potionStorage, that.x - this.game.camera.x, that.y - 40 - this.game.camera.y);
             
             
             ctx.font = PARAMS.DEFAULT_FONT;
@@ -130,3 +128,6 @@ class Chest {
     };
 
 };
+
+
+// Money money money moneyyyy...
