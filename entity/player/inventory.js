c396db1a4944a280ef85895fccf95996affb253f
Ninjasaws <44685782+Ninjasaws@users.noreply.github.com>
2022-02-12 18:30:10 -0800

    Player diamond currency added

--- entity/player/inventory.js
+++ entity/player/inventory.js
@@ -3,11 +3,13 @@ class Inventory {
         Object.assign(this, {game});
         const STARTING_ARROWS = 50;
         const STARTING_POTIONS = 5;
+        const STARTING_DIAMONDS = 0;
         this.maxStack = 99;
 
         //inventory
         this.arrows = STARTING_ARROWS;
         this.potions = STARTING_POTIONS;
+        this.diamonds = STARTING_DIAMONDS;
 
     };
 
@@ -20,6 +22,10 @@ class Inventory {
         if(this.potions > this.maxStack) {
             this.potions = this.maxStack;
         }
+
+        if(this.diamonds > this.maxStack) {
+            this.diamonds = this.maxStack;
+        }
     };
 
     draw(ctx) {
@@ -27,6 +33,7 @@ class Inventory {
         ctx.fillStyle = "White";
         ctx.fillText("🏹 x" + this.arrows, 5, (PARAMS.HEART_DIM * PARAMS.GUI_SCALE) * 2 + 10);
         ctx.fillText("⚗️ x" + this.potions, 130, (PARAMS.HEART_DIM * PARAMS.GUI_SCALE) * 2 + 10);
+        ctx.fillText("💎 x" + this.diamonds, 5, (PARAMS.HEART_DIM * PARAMS.GUI_SCALE) * 2 + 50);
         ctx.font = PARAMS.DEFAULT_FONT;
     };
 };
