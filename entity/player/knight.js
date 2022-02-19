12e9339ef9a133a4decf8a22ff4494dbe410056e
TemporalCatcher <pezzaliembert@hotmail.com>
2022-02-19 00:09:17 -0800

    Switch crouch facing shooting
    Allow crouching to also switch facing when shooting.

--- entity/player/knight.js
+++ entity/player/knight.js
@@ -513,45 +513,41 @@ class Knight extends AbstractPlayer {
 
         } else if (!this.game.attack && this.game.shoot && !this.game.roll) { //only shoot an arrow when not attacking
             if (this.myInventory.arrows > 0 || this.myInventory.arrows == 0 && this.arrow) {
-                if (this.crouch) {
+                if (this.crouch) 
                     this.action = this.states.crouch_shoot;
-                } else {
-                    this.action = this.states.shoot;
-                    let time = this.animations[this.facing][this.action].elapsedTime;
-
-                    //only adjust the direction if using the mouse to shoot
-                    if (!this.game.shootButton) {
-                        let x = this.game.mouse.x + this.game.camera.x;
-                        if (x < this.x + this.width / 2)
-                            this.facing = this.dir.left;
-                        if (x > this.x + this.width / 2)
-                            this.facing = this.dir.right;
-                    }
-                    this.animations[this.facing][this.action].elapsedTime = time;
-                }
-
+                else this.action = this.states.shoot;
             }
             else {
-                if (this.game.down || this.touchHole()) {
+                if (this.game.down || this.touchHole()) 
                     this.action = this.states.crouch_pluck;
-                }
                 else this.action = this.states.pluck;
             }
             let done = this.animations[this.facing][this.action].isDone();
 
+            // shoot arrow
             if (this.animations[this.facing][this.action].currentFrame() == 2 && !this.arrow) {
                 if (this.myInventory.arrows > 0)
                     this.arrow = true;
                 super.shootArrow();
             }
+
+            //only adjust the direction if using the mouse to shoot
+            let time = this.animations[this.facing][this.action].elapsedTime;
+            if (!this.game.shootButton) {
+                let x = this.game.mouse.x + this.game.camera.x;
+                if (x < this.x + this.width / 2)
+                    this.facing = this.dir.left;
+                if (x > this.x + this.width / 2)
+                    this.facing = this.dir.right;
+            }
+            this.animations[this.facing][this.action].elapsedTime = time;
+            
             if (done) {
                 this.action = this.game.down || this.touchHole() ? this.states.crouch : this.DEFAULT_ACTION; //back to idle; added case for crouch attacks
                 this.game.shoot = false;
                 this.game.shootButton = false;
                 this.arrow = false;
             }
-
-
         } else {
             //crouch attack
             this.resetAnimationTimers(this.states.crouch_atk);
