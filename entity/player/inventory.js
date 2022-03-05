807cd86cbced006056bea9d67755e0c4c8fe49cc
David Shcherbina <ninjasaws@gmail.com>
2022-03-05 00:47:18 -0800

    initial "inventory hub" view
    - Give a visual of player upgrades outside of the shop
    - Support items (attack, arrow, and armor upgrade)

--- entity/player/inventory.js
+++ entity/player/inventory.js
@@ -2,6 +2,9 @@ class Inventory {
     constructor(game) {
         Object.assign(this, {game});
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/environment/dark_castle_tileset.png");
+        this.attack_sheet = ASSET_MANAGER.getAsset("./sprites/GUI/attack.png");
+        this.arrow_sheet = ASSET_MANAGER.getAsset("./sprites/projectile/arrows.png");
+        this.armor_sheet = ASSET_MANAGER.getAsset("./sprites/GUI/armor.png");
 
         const STARTING_ARROWS = 50;
         const STARTING_POTIONS = 5;
@@ -14,7 +17,7 @@ class Inventory {
         this.maxUpgrade = 4;
         this.maxxed = false;
 
-        //inventory
+        // inventory
         this.arrows = STARTING_ARROWS;
         this.potions = STARTING_POTIONS;
         this.diamonds = STARTING_DIAMONDS;
@@ -23,9 +26,8 @@ class Inventory {
         this.arrowUpgrade = STARTING_ARROW_UPGRADE;
         this.armorUpgrade = STARTING_ARMOR_UPGRADE;
 
-       
+        // animations
         this.diamond = [];
-
         this.diamond[0] = new Animator(this.spritesheet, 19, 84, 10, 8, 1, 0, 0, false, false, false);
         this.diamond[1] = new Animator(this.spritesheet, 35, 84, 10, 8, 1, 0, 0, false, false, false);
         this.diamond[2] = new Animator(this.spritesheet, 51, 84, 10, 8, 1, 0, 0, false, false, false);
@@ -33,8 +35,28 @@ class Inventory {
         this.diamond[4] = new Animator(this.spritesheet, 83, 84, 10, 8, 1, 0, 0, false, false, false);
         this.diamond[5] = new Animator(this.spritesheet, 99, 84, 10, 8, 1, 0, 0, false, false, false);
 
-        this.timerx4 = 0;
-        this.secondsx4 = 0;
+        this.attack = [];
+        this.attack[0] = new Animator(this.attack_sheet, 0, 0, 45, 45, 1, 0, 0, false, false, false);
+        this.attack[1] = new Animator(this.attack_sheet, 45, 0, 45, 45, 1, 0, 0, false, false, false);
+        this.attack[2] = new Animator(this.attack_sheet, 90, 0, 45, 45, 1, 0, 0, false, false, false);
+        this.attack[3] = new Animator(this.attack_sheet, 135, 0, 45, 45, 1, 0, 0, false, false, false);
+        this.attack[4] = new Animator(this.attack_sheet, 180, 0, 45, 45, 1, 0, 0, false, false, false);
+
+        this.arrow = [];
+        this.arrow[0] = new Animator(this.arrow_sheet, 38, -4, 36, 30, 1, 0, 0, false, false, false);
+        this.arrow[2] = new Animator(this.arrow_sheet, 36, 34, 36, 30, 1, 0, 0, false, false, false);
+        this.arrow[3] = new Animator(this.arrow_sheet, 38, 73, 36, 30, 1, 0, 0, false, false, false);
+        this.arrow[4] = new Animator(this.arrow_sheet, 34, 115, 36, 30, 1, 0, 0, false, false, false);
+        this.arrow[1] = new Animator(this.arrow_sheet, 36, 153, 36, 30, 1, 0, 0, false, false, false);
+
+        this.armor = [];
+        this.armor[0] = new Animator(this.armor_sheet, 60, 0,    64, 64, 1, 0, 0, false, false, false);
+        this.armor[1] = new Animator(this.armor_sheet, 60, 64,   64, 64, 1, 0, 0, false, false, false);
+        this.armor[2] = new Animator(this.armor_sheet, 60, 128,  64, 64, 1, 0, 0, false, false, false);
+        this.armor[3] = new Animator(this.armor_sheet, 60, 192,  64, 64, 1, 0, 0, false, false, false);
+
+        this.timer = 0;
+        this.seconds = 0;
     };
 
     copyInventory(inventory) {
@@ -50,10 +72,9 @@ class Inventory {
     };
 
     update() {
-        this.timerx4 += this.game.clockTick*4;
-        if(this.timerx4 > 1){
-             this.secondsx4++;
-             this.timerx4--;
+        this.timer += this.game.clockTick;
+        if(this.timer > 1){
+             this.seconds++;
         }
 
         if(this.arrows > this.maxStack) {
@@ -86,9 +107,22 @@ class Inventory {
         ctx.font = PARAMS.BIG_FONT;
         ctx.fillStyle = "White";
         ctx.fillText("🏹 x" + this.arrows, 5, (PARAMS.HEART_DIM * PARAMS.GUI_SCALE) * 2 + 10);
-        ctx.fillText("⚗️ x" + this.potions, 130, (PARAMS.HEART_DIM * PARAMS.GUI_SCALE) * 2 + 10);
+        ctx.fillText("⚗️ x" + this.potions, 150, (PARAMS.HEART_DIM * PARAMS.GUI_SCALE) * 2 + 10);
         ctx.fillText("💎 x" + this.diamonds, 7, (PARAMS.HEART_DIM * PARAMS.GUI_SCALE) * 2 + 50 -1);
-        this.diamond[this.secondsx4%6].drawFrame(this.game.clockTick, ctx, 8, 133, 2.5);
+        this.diamond[Math.floor(this.timer / 0.25) % 6].drawFrame(this.game.clockTick, ctx, 8, 133, 2.5);
+
+        ctx.font = ctx.font.replace(/\d+px/, "14px");
+        
+        this.attack[Math.floor(this.timer / .5) % 5].drawFrame(this.game.clockTick, ctx, 8, 200, 1);
+        ctx.fillText("x(" + (this.attackUpgrade+1) + "/" + (this.maxUpgrade + 1) + ")", 8 + 55, 200 + 35);
+
+        this.arrow[this.arrowUpgrade].drawFrame(this.game.clockTick, ctx, 8, 250, 1.2);//1.7
+        ctx.fillText("x(" + (this.arrowUpgrade+1) + "/" + (this.maxUpgrade + 1) + ")", 8 + 55, 250 + 30);
+
+        this.armor[this.armorUpgrade].drawFrame(this.game.clockTick, ctx, 8, 300, .6);
+        ctx.fillText("x(" + (this.armorUpgrade+1) + "/" + 4 + ")", 8 + 55, 300 + 24);
+        
+
         ctx.font = PARAMS.DEFAULT_FONT;
     };
 };
