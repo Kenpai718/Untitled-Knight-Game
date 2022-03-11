c3e31876e5e768100f9cabbf5a2428d6cbfed882
TemporalCatcher <pezzaliembert@hotmail.com>
2022-03-10 22:40:24 -0800

    Fix Portal Sizing
    Fix portal size issue

--- entity/level/portal.js
+++ entity/level/portal.js
@@ -3,7 +3,7 @@ class Portal extends AbstractInteractable {
         super(game, x, y);
         this.spritesheet = ASSET_MANAGER.getAsset("./sprites/environment/portal.png");
         this.scale = PARAMS.BLOCKDIM;
-        this.animation = new Animator(this.spritesheet, 0, 0, 320, 320, 40, 0.1, 0, false, true, false);
+        this.animation = new Animator(this.spritesheet, 0, 0, 32, 32, 40, 0.1, 0, false, true, false);
         this.states = { idle: 0, notIdle: 1};
         this.state = this.states.notIdle;
         this.updateBB();
@@ -30,10 +30,10 @@ class Portal extends AbstractInteractable {
 
     draw(ctx) {
         if (this.state == this.states.idle) {
-            ctx.drawImage(this.spritesheet, 0, 0, 320, 320, this.x * this.scale - this.game.camera.x, this.y * this.scale - this.game.camera.y, this.scale * 3, this.scale * 3);
+            ctx.drawImage(this.spritesheet, 0, 0, 32, 32, this.x * this.scale - this.game.camera.x, this.y * this.scale - this.game.camera.y, this.scale * 3, this.scale * 3);
         } else {
             //console.log("fjei");
-            this.animation.drawFrame(this.game.clockTick, ctx, this.x * this.scale - this.game.camera.x, this.y * this.scale - this.game.camera.y, this.scale * 3);
+            this.animation.drawFrame(this.game.clockTick, ctx, this.x * this.scale - this.game.camera.x, this.y * this.scale - this.game.camera.y, this.scale / 32 * 3);
         }
 
     };
