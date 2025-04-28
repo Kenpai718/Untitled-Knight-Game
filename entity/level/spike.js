d38edb020ea4cb5cc57a364178c9be00b6601570
chq-kennetha <kenneth.ahrens@expeditors.com>
2025-04-28 14:59:32 -0700

    reduce spike damage

--- entity/level/spike.js
+++ entity/level/spike.js
@@ -10,18 +10,19 @@ class Spike extends AbstractBackFeature {
         this.srcW = 15;
         this.srcH = 16;
         this.BB = new BoundingBox(this.x * this.scale, (this.y + 1 / 2) * this.scale, this.w * this.scale, this.scale / 2);
+        this.spikeDmg = 5;
     };
 
     update() {
         var self = this;
         this.game.entities.forEach(function (entity) {
             if (entity.BB && self.BB.collide(entity.BB) && entity instanceof AbstractEntity) {
-                entity.takeDamage(10, false);
+                entity.takeDamage(this.spikeDmg, false);
             }
         });
         this.game.enemies.forEach(function (entity) {
             if (entity.BB && self.BB.collide(entity.BB) && entity instanceof AbstractEntity) {
-                entity.takeDamage(5, false);
+                entity.takeDamage(this.spikeDmg, false);
                 entity.setDamagedState();
             }
         });
