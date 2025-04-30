522a10361e2a7de0e712763c48a9dba1d08a6f94
Ken Ahrens <75004340+Kenpai718@users.noreply.github.com>
2025-04-30 02:37:19 -0700

    remove player knockback method

--- entity/player/abstractplayer.js
+++ entity/player/abstractplayer.js
@@ -551,11 +551,7 @@ class AbstractPlayer extends AbstractEntity {
     }
 
     takeKnockback(damage) {
-        if(this.facing === this.dir.right) {
-            this.velocity.x = -(BASE_KNOCKBACK + ((damage / 100) * 1000));
-        } else {
-            this.velocity.x = BASE_KNOCKBACK  + ((damage / 100) * 1000);
-        }
+        //leaving empty because I think knockback for player would be annoying
     }
 
     /**Collision helper methods */
